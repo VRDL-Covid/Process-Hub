abd554b2e2c173bd10c79f2679f432d1aa484a54
markf06 <markfranklin346@gmail.com>
2020-04-03 18:49:37 +0100

    Added marker assets (mix of models, prefabs and code) to automatically assess dimension from the real world

---
+++ Assets/Scripts/Dimensionizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dimensionizer : MonoBehaviour
+{
+    [Range(0,2)]
+    public int dimChooser;
+
+    public GameObject dimension;
+
+    public Color lengthTextFaceColor = new Color(1, 1, 1);
+    public string shaderColProperty = "_FaceColor";
+
+    public TMPro.TextMeshPro lengthTextBox, lengthRayCastTextBox;
+
+    public Vector3 scalerSource = new Vector3(0,0,0);
+
+    void Start()
+    {
+        if (null == dimension)
+            dimension = gameObject.transform.GetChild(0).gameObject;
+        if (null == lengthTextBox)
+        {
+            lengthTextBox = gameObject.transform.GetChild(1).gameObject.transform.GetChild(0).GetComponent<TMPro.TextMeshPro>();
+            lengthRayCastTextBox = gameObject.transform.GetChild(1).gameObject.transform.GetChild(1).GetComponent<TMPro.TextMeshPro>();
+        }
+        dimension.transform.localScale = scalerSource;
+
+        lengthTextBox.text = string.Format("{0:0.0}m", scalerSource[dimChooser]);
+        lengthTextBox.renderer.material.SetColor(shaderColProperty, lengthTextFaceColor);
+    }
+
+        void Update()
+    {
+        // do raycast to hit mesh....
+
+    }
+}
